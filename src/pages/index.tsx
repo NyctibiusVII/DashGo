352a9a737fef18dc1c21222f7b209fe3801940c8
NyctibiusVII <matheus.dev.07@gmail.com>
2021-12-09 14:29:11 -0300

    Base configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <Head><title>DashGo</title></Head>
+            <div className=''>
+
+            </div>
+        </>
+    )
+}
+export default Home
